147f41a10accf44e807cab90321bf0add188d5f9
Johnny Domino <jvdomino@gmail.com>
2014-03-08 11:04:00 -0500

    improve & document hello-bot

--- bots/hello.js
+++ bots/hello.js
@@ -1,17 +1,34 @@
 module.exports = {
   path:    '/hello',
-  handler: function(request,reply) {
-    var slackData = request.payload;
+  handler: function(request, reply) {
 
-    if (   slackData.text
-        && slackData.text.match(/hello/)) {
+    var msg = request.payload;
 
+    // Suppose you only want to respond to messages that match a certain criteria
+    if (   msg.text
+        && msg.text.match(/\bhello\b/)) {
+
+      // Empty bodies or bodies with an empty text property will
+      // simply be ignored.
+      var body = 'Herrrro there!';
+
+      // Non-200 responses will be retried a reasonable number of times.
+      var status = 200; 
+
+      // If you would like to change the name on a per-response basis,
+      // simply include a `username` property in your response.
+      var username = 'hello-bot';
+
+      // Respond to Slack
       reply({
-        "text": "Hello.",
-        "username": "jbot"
-      });
+        'text':     body,
+        'username': username
+      }).code(status);
     
-    } else { reply().code(204); }
+    } else { 
 
+      // We're not interested in this event.
+      reply().code(204);
+    }
   }
-}
+};
